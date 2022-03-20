632e00abcffed0e52ddac3df1f26c692c5fc0d69
Inaopeee <inaopeee@gmail.com>
2022-03-20 14:48:12 +0900

    update: show blog links to index.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
+import Date from '../../components/date';
 import Head from 'next/head';
 import Layout from "../../components/layout";
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 const Post = ({ postData }) => {
@@ -8,12 +10,13 @@ const Post = ({ postData }) => {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}/>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
     </Layout>
   );
 }
